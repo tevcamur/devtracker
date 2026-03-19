79305be82761c69df0aa1b55b9ea7d45648867dd
tevcamur <tevcamur@outlook.com>
2026-03-19 12:21:19 +0100

    Initial Spring Boot project setup with Maven and Java 21

---
+++ src/main/java/com/tevcode/devtracker/DevtrackerApplication.java
@@ -0,0 +1,13 @@
+package com.tevcode.devtracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevtrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevtrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tevcode/devtracker/DevtrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tevcode.devtracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevtrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
